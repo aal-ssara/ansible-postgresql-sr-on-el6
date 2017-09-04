75a14928c887e3ef93dffb946bba0095331b58a9
Ander Astudillo <ander.astudillo@surfsara.nl>
2017-09-04 15:25:11 +0200

    Plugin to lookup IP's by hostname

---
+++ lookup_plugins/ip.py
@@ -0,0 +1,20 @@
+# from https://stackoverflow.com/questions/32324120/arbitrary-host-name-resolution-in-ansible
+
+import ansible.utils as utils
+import ansible.errors as errors
+from ansible.plugins.lookup import LookupBase
+import socket
+
+class LookupModule(LookupBase):
+
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, variables=None, **kwargs):
+
+        hostname = terms[0]
+
+        if not isinstance(hostname, basestring):
+            raise errors.AnsibleError("ip lookup expects a string (hostname)")
+
+        return [socket.gethostbyname(hostname)]
